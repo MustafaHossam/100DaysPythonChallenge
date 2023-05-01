454d52164b08d452c6fd22c90f6477b7515c9b0e
MustafaHossam <44845362+MustafaHossam@users.noreply.github.com>
2023-05-01 23:16:48 +0300

    DAY1 Challenge Brand Name Generator

---
+++ DAY_1/brandNameGenerator.py
@@ -0,0 +1,7 @@
+print("Wlecome to the Band Name Generator")
+#print('what\'s the City you grew up in? ')
+city = input('what\'s the City you grew up in?\n')
+#print('what\'s your pet\'s name?')
+# print(type(city)) " input func takes input as str "
+petName = input('what\'s your pet\'s name?\n')
+print("your brand name could be "+ city +" "+ petName)
